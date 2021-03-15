51241733ee20e500f4e06d380d5bc19ba94ab700
Eldors <safonovpaspb@gmail.com>
2021-03-15 20:00:17 +0300

    comparison using classes

--- JS/main.js
+++ JS/main.js
@@ -18,15 +18,15 @@ function j() {
     let i = 0;
     let arrayPairs = generatedArrayPairs();
     while ( i < flipContainer.length ) {
-        let cardNumber = allocation();
+        let cardNumber = allocationNumbersCard();
         flipContainer[cardNumber].firstElementChild.lastElementChild.innerText = arrayPairs[i];
         i++; 
     }
 };
 
-function allocation() {
+function allocationNumbersCard() {
     let generatedNumberCard = getRandomInt(20);
-    if(flipContainer[generatedNumberCard].firstElementChild.lastElementChild.textContent != '') return allocation();
+    if(flipContainer[generatedNumberCard].firstElementChild.lastElementChild.textContent != '') return allocationNumbersCard();
     return generatedNumberCard;
 }
 
@@ -38,9 +38,49 @@ function getRandomInt(max) {
 
 function flipCard(event) {
     if(!event.target.matches('section.test div.flip-container')) return;
-    event.target.classList.toggle('flip');
-    setTimeout(function (){ event.target.classList.toggle('flip')}, 2000, event.target);
+    
+    //let openCard = document.querySelector('.flip');
+    //if(openCard != null) console.log(openCard.firstElementChild.lastElementChild.textContent);
+
+    comparisonOfNumberCard(event.target)
+    //event.target.classList.add('flip');
+
+
+    //setTimeout(function (){ event.target.classList.remove('flip')}, 1000);
 }
+
+function comparisonOfNumberCard(card) {
+    let openCard = document.querySelector('.flip-container.flip');
+    if(openCard != null) {
+        card.classList.add('flip');
+        if(openCard.firstElementChild.lastElementChild.textContent == card.firstElementChild.lastElementChild.textContent) {
+            /*openCard.classList.add('open');
+            card.classList.add('open');            */
+            let a = document.createElement('div');
+            a.textContent = openCard.firstElementChild.lastElementChild.textContent;
+            //console.log('a.textContent: ', a.textContent);
+            //a.classList.add('open');
+            openCard.classList.add('open');
+            card.classList.add('open');
+            //openCard.insertBefore(a, openCard.firstElementChild);
+            //openCard.classList.add('open');
+            //openCard.classList.remove('flip');
+            //card.insertBefore(a, card.firstElementChild); 
+
+        }
+        else {
+            card.classList.add('flip');
+            setTimeout(function (){
+                card.classList.remove('flip');
+                openCard.classList.remove('flip');    
+            }, 500);
+        }
+    }
+    else {
+        card.classList.add('flip');
+        setTimeout(function (){ card.classList.remove('flip')}, 2000);
+    }
+};
 
 test.addEventListener('click', flipCard);
 
