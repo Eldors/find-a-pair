a7d5da2c82423a9e50f1c8d1ca756bf37a4298f7
Eldors <safonovpaspb@gmail.com>
2021-05-08 17:33:32 +0300

    works but needs verification

--- v0.1/JS/main.js
+++ v0.1/JS/main.js
@@ -24,176 +24,6 @@ const arrId = [
     '925b65bd-3af3-4496-be20-dd5af156c2d5'
 ];
 
-// class Area {
-
-//     constructor() {
-//         this.playingField = document.querySelector('.playing-field');
-//         this.arrCell = [];
-//         this.arrayPairs = [];
-//         this.ev1 = ""; // undefined
-//         this.ev2 = "";
-//         this.playingField.addEventListener('click', (event) => {
-//             // отдельная функция
-//             if (!event.target.matches('div.playing-field div.back div.blur')) {
-//                 return;
-//             }
-//             this.checkingTheEventNumber(event.target);
-//             //this.checkСoincidence(event.target);
-//         });
-//         this.createArrayPairs();
-//         this.createGrid();
-//         this.allocationNumbers();
-//     }
-
-//     isClickableBlock() {
-
-//     }
-
-//     createGrid() {
-//         for (let i = 0; i < 20; i++) {
-//             creatCell(this.playingField);
-//         }
-//     }
-
-//     allocationNumbers() {
-//         let i = 0;
-//         const valueCell = document.querySelectorAll('p.value-cell');
-//         // valueCell.forEach((value, idx) => {
-//         //     const generationNumber = this.generationRandomInt(20);
-
-//         //     // ===
-//         //     if (valueCell[generationNumber].textContent == '') {
-//         //         valueCell[generationNumber].textContent = this.arrayPairs[idx];
-//         //     }
-//         // });
-
-//         while (i < valueCell.length) {
-//             const generationNumber = generationRandomInt(20);
-
-//             // ===
-//             if (valueCell[generationNumber].textContent === '') {
-//                 valueCell[generationNumber].textContent = this.arrayPairs[i];
-//                 i++;
-//             }
-//         }
-//     }
-
-//     toggleFilter(e) {
-//         e.classList.toggle('filter');
-//     }
-
-//     // checkСoincidence(event) {
-//     //     this.ev = event;
-//     //     this.setTimer(event);
-//     //     this.toggleFilter(event);
-//     //     this.checkOpenCard(event);
-//     // }
-
-//     // checkOpenCard(e) {
-//     //     const openCard = this.getOpenCard(e);
-//     //     if (openCard) {
-//     //         console.log(openCard);
-//     //     }
-//     // }
-
-//     // getOpenCard(e) {
-//     //     console.log(this.arrCell[0].blur.className == 'blur');
-//     //     let openCard = '';
-//     //     while (openCard == '') {
-//     //         for (let i = 0; i < this.arrCell.length; i++) {
-//     //             if (this.arrCell[i].blur.className == 'blur') {
-//     //                 openCard = this.arrCell[i];
-//     //             }
-//     //             i++;
-//     //         }
-//     //     }
-//     //     return openCard;
-//     // }
-
-//     checkingTheEventNumber(event) {
-//         if (this.ev1 == '') {
-//             this.ev1 = event;
-//             this.toggleFilter(this.ev1);
-//             this.setTimer1(this.ev1);
-//         } else if (this.ev2 == '') {
-//             this.ev2 = event;
-//             this.toggleFilter(this.ev2);
-//             this.setTimer2(this.ev2);
-//             this.checkMath();
-//         } else {
-//             this.toggleFilter(this.ev1);
-//             this.toggleFilter(this.ev2);
-//             this.clearTimer(this.evTimerId1);
-//             this.clearTimer(this.evTimerId2);
-//             this.ev1 = '';
-//             this.ev2 = '';
-//         }
-//     }
-
-//     checkMath() {
-//         if (this.ev1.parentNode.lastElementChild.textContent == this.ev2.parentNode.lastElementChild.textContent) {
-//             const blur1 = this.ev1.parentNode.firstElementChild,
-//                 blur2 = this.ev2.parentNode.firstElementChild;
-//             this.ev1.parentNode.removeChild(blur1);
-//             this.ev2.parentNode.removeChild(blur2);
-//         } else {
-//             this.toggleFilter(this.ev1);
-//             this.toggleFilter(this.ev2);
-//         }
-//         this.clearTimer(this.evTimerId1);
-//         this.clearTimer(this.evTimerId2);
-//         this.ev1 = '';
-//         this.ev2 = '';
-//     }
-
-//     setTimer1(ev1) {
-//         this.ev1TimerId = setTimeout((ev1) => {
-//             this.toggleFilter(ev1);
-//             this.ev1 = '';
-//         }, 2000, ev1);
-//     }
-
-//     setTimer2(ev2) {
-//         this.ev2TimerId = setTimeout((ev2) => {
-//             this.toggleFilter(ev2);
-//             this.ev2 = '';
-//         }, 2000, ev2);
-//     }
-
-//     clearTimer(evTimerId) {
-//         clearTimeout(evTimerId);
-//     }
-
-//     createArrayPairs() {
-//         let k = 0;
-//         while (k < 10) {
-//             this.arrayPairs.push(k, k);
-//             k++;
-//         }
-//     }
-
-//     generationRandomInt(maxNumber) {
-//         return Math.floor(Math.random() * Math.floor(maxNumber));
-//     }
-// }
-
-// function creatCell(parent) {
-//     const container = document.createElement('div'),
-//         back = document.createElement('div'),
-//         blur = document.createElement('div'),
-//         valueCell = document.createElement('p');
-
-//     container.classList.add('container');
-//     back.classList.add('back');
-//     blur.classList.add('blur', 'filter');
-//     valueCell.classList.add('value-cell', 'unselectable');
-
-//     parent.appendChild(container);
-//     container.appendChild(back);
-//     back.appendChild(blur);
-//     back.appendChild(valueCell);
-// }
-
 class GameManager {
     constructor() {
         this.playingFieldSize = 20;
@@ -204,7 +34,6 @@ class GameManager {
     creatingPlayingField() {
         this.playingField = new PlayingField(this.playingFieldSize);
     }
-
 }
 
 class PlayingField {
@@ -212,6 +41,7 @@ class PlayingField {
         this.playingField = document.querySelector('div.playing-field');
         this.playingFieldSize = size;
         this.arrayCell = [];
+        this.timerOfTheFirstEvent = undefined;
 
         this.randomlyDistributeNumbers();
         this.creatingCells();
@@ -219,7 +49,6 @@ class PlayingField {
         this.appendFragment();
         this.addDataIdCells();
         this.addEventListenerPlayingField();
-        console.log(this.arrayCell);
     }
 
     creatingCells() {
@@ -279,35 +108,76 @@ class PlayingField {
 
     addEventListenerPlayingField() {
         this.playingField.addEventListener('click', (event) => {
-
-            const cellN = arrId.find( (id) => {
-                return id === event.target.dataset.id;
+            const eventCellIndex = arrId.findIndex( (index, ) => {
+                if (index === event.target.dataset.id) {
+                    return index;
+                }
             });
-            console.log('cellN', cellN);
-
-            // for( let i = 0; i < this.playingFieldSize; i++) {
-            //     if( event.target.dataset.id === arrId[i]) {
-            //         this.eventClick = this.arrayCell[i];
-            //         this.openCell(this.arrayCell[i]);
-            //         return;
-            //     }
-            // }
+            this.timerLogic(this.arrayCell[eventCellIndex]);
         });
+    }
+
+    timerLogic(eventCell) {
+        if(this.checkActivityCell(eventCell)) {
+            return;
+        }
+
+        if(this.checkTimerStatus()) {
+            this.firstOpenCell = eventCell;
+            this.openCell(this.firstOpenCell);
+            this.startTheTimerOfTheFirstEvent();
+        }
+        else if(this.checkTimerStatus() === false) {
+            this.secondOpenCell = eventCell;
+            this.openCell(this.secondOpenCell);
+            this.clearTheTimerOfTheFirstEvent();
+            if( this.checkMatchOpenCell()) {
+                this.firstOpenCell.disableCell();
+                this.secondOpenCell.disableCell();
+                this.firstOpenCell = undefined;
+                this.secondOpenCell = undefined;
+            } else {
+                this.startErrorTimer(this.firstOpenCell, this.secondOpenCell);
+                this.firstOpenCell = undefined;
+                this.secondOpenCell = undefined;
+            }  
+        }
+    }
+
+    checkActivityCell(eventCell) {
+        return eventCell.disabled;
     }
 
     openCell(cell) {
         cell.switchVisibility();
-        this.addTimerEvent(cell);
-        
     }
 
-    addTimerEvent(cell) {
-        const timer = new Timer(cell);
-        timer.setTimerCell();
-        setTimeout(() => {
-            console.log(timer);
+    checkTimerStatus() {
+        return this.timerOfTheFirstEvent === undefined;
+    }
+
+    startTheTimerOfTheFirstEvent() {
+        this.timerOfTheFirstEvent = setTimeout(() => {
+            this.firstOpenCell.switchVisibility();
+            this.firstOpenCell = undefined;
+            this.timerOfTheFirstEvent = undefined;
         }, 2000);
-        
+    }
+
+    clearTheTimerOfTheFirstEvent() {
+        clearTimeout(this.timerOfTheFirstEvent);
+        this.timerOfTheFirstEvent = undefined;
+    }
+
+    startErrorTimer(firstCell, secondCell) {
+        this.errorTimer = setTimeout( () => {
+            firstCell.switchVisibility();
+            secondCell.switchVisibility();
+        }, 500);
+    }
+
+    checkMatchOpenCell() {
+        return this.secondOpenCell.container.dataset.value === this.firstOpenCell.container.dataset.value;
     }
 }
 
@@ -319,6 +189,7 @@ class Cell {
     }
 
     disableCell() {
+        this.disabled = true;
         this.container.dataset.disabled = true;
     }
 
@@ -341,9 +212,10 @@ class Cell {
 
         shirt.setAttribute('src', '/pictures/shirt.svg');
         valueCell.setAttribute('src', `/pictures/numbers${this.value}.png`);
+        container.dataset.value = this.value;
 
         container.dataset.disabled = this.disabled;
-        container.dataset.visibility = this.visibility;
+        // container.dataset.visibility = this.visibility;
 
         container.appendChild(shirt);
         container.appendChild(valueCell);
@@ -366,8 +238,6 @@ class Timer {
             return false;
         }, 2000);
     }
-
-
 }
 
 const gameManager = new GameManager();
